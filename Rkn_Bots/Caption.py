09a2565d1c2907fafbdfedf6d872720842c74d5d
DemonslayerTheEvil <130355084+DemonslayerTheEvil@users.noreply.github.com>
2024-10-17 22:56:00 +0530

    Update Caption.py

--- Rkn_Bots/Caption.py
+++ Rkn_Bots/Caption.py
@@ -1,30 +1,40 @@
+# Rkn Developer
+# Don't Remove Credit 😔
+# Telegram Channel @RknDeveloper & @Rkn_Botz
+# Developer @RknDeveloperr
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import Client, filters, errors, types
 from config import Rkn_Bots
 import asyncio, re, time, sys, os
 from .database import total_user, getid, delete, addCap, updateCap, insert, chnl_ids
 from pyrogram.errors import FloodWait
 
-# Command to view bot users
-@Client.on_message(filters.private & filters.user(Rkn_Bots.ADMIN) & filters.command(["rknusers"]))
+@Client.on_message(filters.private & filters.user(Rkn_Bots.ADMIN) & filters.command(["users"]))
 async def all_db_users_here(client, message):
     start_t = time.time()
     rkn = await message.reply_text("Processing...")
-    uptime = time.strftime("%Hh%Mm%Ss", time.gmtime(time.time() - client.uptime))    
     total_users = await total_user()
-    end_t = time.time()
-    time_taken_s = (end_t - start_t) * 1000
-    await rkn.edit(text=f"**--Bot Processed--** \n\n**Bot Started UpTime:** {uptime} \n**Bot Current Ping:** `{time_taken_s:.3f} ms` \n**All Bot Users:** `{total_users}`")
+    user_list = await getid()
+    
+    user_info = "\n".join(
+        [f"👤 Name: <a href='tg://user?id={user['_id']}'>{user['first_name']}</a>\n🕵🏻‍♂️ Username: @{user['username']}\n" for user in user_list])
 
-# Command for broadcasting messages to users
+    await rkn.edit(
+        f"**--Bot Users--** \n\n**Total Users:** `{total_users}`\n\n{user_info}")
+
 @Client.on_message(filters.private & filters.user(Rkn_Bots.ADMIN) & filters.command(["broadcast"]))
 async def broadcast(bot, message):
     if message.reply_to_message:
-        rkn = await message.reply_text("Bot Processing. Checking all bot users.")
+        rkn = await message.reply_text("Bot Processing.\nI am checking all bot users.")
         all_users = await getid()
         tot = await total_user()
-        success, failed, deactivated, blocked = 0, 0, 0, 0
-        await rkn.edit(f"Broadcasting started...")
-        
+        success = 0
+        failed = 0
+        deactivated = 0
+        blocked = 0
+        await rkn.edit(f"bot broadcasting started...")
         async for user in all_users:
             try:
                 time.sleep(1)
@@ -39,55 +49,92 @@ async def broadcast(bot, message):
             except Exception as e:
                 failed += 1
                 await delete({"_id": user['_id']})
-            
+                pass
             try:
-                await rkn.edit(f"<u>Broadcast Progress</u>\n\n• Total Users: {tot}\n• Successful: {success}\n• Blocked Users: {blocked}\n• Deactivated Accounts: {deactivated}\n• Unsuccessful: {failed}")
+                await rkn.edit(f"<u>Broadcast Processing</u>\n\n• Total users: {tot}\n• Successful: {success}\n• Blocked users: {blocked}\n• Deleted accounts: {deactivated}\n• Unsuccessful: {failed}")
             except FloodWait as e:
                 await asyncio.sleep(e.x)
+        await rkn.edit(f"<u>Broadcast Completed</u>\n\n• Total users: {tot}\n• Successful: {success}\n• Blocked users: {blocked}\n• Deleted accounts: {deactivated}\n• Unsuccessful: {failed}")
 
-        await rkn.edit(f"<u>Broadcast Completed</u>\n\n• Total Users: {tot}\n• Successful: {success}\n• Blocked Users: {blocked}\n• Deactivated Accounts: {deactivated}\n• Unsuccessful: {failed}")
 
-# Command to restart the bot
 @Client.on_message(filters.private & filters.user(Rkn_Bots.ADMIN) & filters.command("restart"))
 async def restart_bot(b, m):
-    rkn_msg = await b.send_message(text="🔄 Processes stopped. Restarting bot...", chat_id=m.chat.id)       
+    rkn_msg = await b.send_message(text="**🔄 Processes stopped. Bot is restarting...**", chat_id=m.chat.id)
     await asyncio.sleep(3)
-    await rkn_msg.edit("✅ Bot restarted. You can now use it.")
+    await rkn_msg.edit("**✅ Bot is restarted. You can now use me**")
     os.execl(sys.executable, sys.executable, *sys.argv)
 
-# Command to start the bot
+
+@Client.on_message(filters.command(["set_caption", "set"]) & filters.channel)
+async def setCaption(bot, message):
+    if len(message.command) < 2:
+        return await message.reply(
+            "<b>give me a caption to set</b>\n<u>Example:- ⬇️</u>\n\n<code>/set_caption {file_name}\n\n{file_caption}\n\nsize » {file_size}\n\nJoin :- @your_channel</code>"
+        )
+    chnl_id = message.chat.id
+    caption = (
+        message.text.split(" ", 1)[1] if len(message.text.split(" ", 1)) > 1 else None
+    )
+    chkData = await chnl_ids.find_one({"chnl_id": chnl_id})
+    if chkData:
+        await updateCap(chnl_id, caption)
+        return await message.reply(f"Successfully Updated Your Caption.\n\nYour New Caption: `{caption}`")
+    else:
+        await addCap(chnl_id, caption)
+        return await message.reply(f"Successfully Updated Your Caption.\n\nYour New Caption: `{caption}`")
+
 @Client.on_message(filters.command("start") & filters.private)
 async def start_cmd(bot, message):
     user_id = int(message.from_user.id)
     await insert(user_id)
     await message.reply_photo(
         photo=Rkn_Bots.RKN_PIC,
-        caption=f"<b>Hey, {message.from_user.mention}\n\nI'm an auto-caption bot. I automatically edit captions for videos, audio files, and documents posted on channels.\n\nUse <code>/set_caption</code> to set caption\nUse <code>/delcaption</code> to delete caption and set it to default.\n\nNote: All commands work on channels only.</b>",
-        reply_markup=types.InlineKeyboardMarkup([[
-            types.InlineKeyboardButton('Main Channel', url='https://t.me/RknDeveloper'),
-            types.InlineKeyboardButton('Help Group', url='https://t.me/Rkn_Bots_Support')
-            ],[
-            types.InlineKeyboardButton('🔥 Source Code 🔥', url='https://github.com/RknDeveloper/Rkn-AutoCaptionBot')
-    ]]))
+        caption=f"ʜᴇʏ, {message.from_user.mention}\n\nI ᴀᴍ ᴀ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀᴜᴛᴏᴄᴀᴘᴛɪᴏɴʙᴏᴛ. ᴠᴇʀʏ sɪᴍᴘʟᴇ ᴛᴏ ᴜsᴇ ᴍᴇ. ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ ᴏᴠᴇʀ ᴛʜᴇʀᴇ. ᴛʜᴇɴ sᴇᴛ Yᴏᴜʀ Cᴀᴘᴛɪᴏɴ Bʏ Usɪɴɢ <mono>/set</mono> & <mono>/setCaption</mono> Cᴏᴍᴍᴀɴᴅ ғᴏʀ ᴇɴᴀʙʟɪɴɢ ᴀᴜᴛᴏᴄᴀᴘᴛɪᴏɴ.\n\n"
+                f"<blockquote>ɴᴏᴛᴇ: Mᴀᴋᴇ sᴜʀᴇ I ᴀᴍ ᴀɴ ᴀᴅᴍɪɴ ɪɴ ʏᴏᴜʀ ᴄʜᴀᴛ ᴡɪᴛʜ ᴀᴅᴍɪɴ ʀɪɡʜᴛs.</blockquote>",
+        reply_markup=InlineKeyboardMarkup([[
+            InlineKeyboardButton('♻️ Help', callback_data='help_button')
+        ], [
+            InlineKeyboardButton('❌ Close', callback_data='close_button'),
+            InlineKeyboardButton('🔙 Back', callback_data='back_button')
+        ]]))
+    
+@Client.on_callback_query(filters.regex('help_button'))
+async def help_button_callback(bot, callback_query):
+    await callback_query.answer()  # Acknowledge the callback
+    help_text = (
+        "•••[( Get Help )]•••\n\n"
+        "⚠️ ALTER ⚠️\n"
+        "• add this bot in your channel with all admin permission\n"
+        "• use this command in your channel\n"
+        "• this command works only in channels\n"
+        "• keep file without forward tag\n\n"
+        "•> /set - set new caption in your channel\n"
+        "•> /del - delete your caption\n"
+        "•> /view - view your caption\n\n"
+        "Format:\n"
+        "{file_name} = original file name\n"
+        "{file_caption} = original file caption\n"
+        "{file_size} = file original size\n\n"
+        "Eg:- /set\n"
+        "{file_name} or {file_caption}\n\n"
+        "⚙️ Size » {file_size}\n\n"
+        "╔═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗\n"
+        "💥 𝙹𝙾𝙸𝙽 :- channel link\n"
+        "💥 𝙹𝙾𝙸𝙽 :- channel link\n"
+        "╚═════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝"
+    )
+    await callback_query.message.edit_caption(help_text)
 
-# Command to set a custom caption
-@Client.on_message(filters.command(["set_caption", "set"]) & filters.channel)
-async def set_caption(bot, message):
-    if len(message.command) < 2:
-        return await message.reply(
-            "<b>Provide a caption to set</b>\n<u>Example:</u> ⬇️\n\n<code>/set_caption {file_name}\n\n{file_caption}\n\nsize » {file_size}\n\nJoin :- @your_channel</code>"
-        )
-    chnl_id = message.chat.id
-    caption = message.text.split(" ", 1)[1]
-    chk_data = await chnl_ids.find_one({"chnl_id": chnl_id})
-    if chk_data:
-        await updateCap(chnl_id, caption)
-        return await message.reply(f"Caption updated successfully:\n\n`{caption}`")
-    else:
-        await addCap(chnl_id, caption)
-        return await message.reply(f"Caption added successfully:\n\n`{caption}`")
+@Client.on_callback_query(filters.regex('close_button'))
+async def close_button_callback(bot, callback_query):
+    await callback_query.answer()  # Acknowledge the callback
+    await callback_query.message.delete()  # Close the page
 
-# Command to delete a custom caption
+@Client.on_callback_query(filters.regex('back_button'))
+async def back_button_callback(bot, callback_query):
+    await callback_query.answer()  # Acknowledge the callback
+    await start_cmd(bot, callback_query.message)  # Call start_cmd to go back
+
 @Client.on_message(filters.command(["delcaption", "del_caption", "delete_caption", "del"]) & filters.channel)
 async def del_caption(_, msg):
     chnl_id = msg.chat.id
@@ -110,7 +157,6 @@ async def view_caption(bot, message):
     else:
         return await message.reply("<b>No custom caption set. Using the default caption.</b>")
 
-# Automatically edit captions for files
 @Client.on_message(filters.channel)
 async def auto_edit_caption(bot, message):
     chnl_id = message.chat.id
